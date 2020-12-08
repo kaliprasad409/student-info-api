772ae045b83a67574591d38c697bf36867c809bf
Kali Prasad <kaliprasad409@gmail.com>
2020-12-08 16:09:42 +0530

    message for commit

---
+++ studentinfoapi/studentinfoapi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for studentinfoapi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studentinfoapi.settings')
+
+application = get_asgi_application()
